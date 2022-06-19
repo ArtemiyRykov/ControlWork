1493f6132abbeb103b4d3c398a101a360e685d56
Artemiy <azazellorart@gmail.com>
2022-06-20 01:36:00 +0500

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array = new string[] { "hello", "2", "world", ":-)", "123" };
+string[] newArray = new string[array.Length];
+
+PrintArray(array);
+ResultArray(array, newArray);
+PrintArray(newArray);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ResultArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
